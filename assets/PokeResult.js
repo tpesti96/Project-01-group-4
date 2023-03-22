e58a43a3e0b14778e870fd94dcb529580ecc2bb6
jakefair <100549835+jakefair97@users.noreply.github.com>
2023-03-21 20:35:34 -0500

    gif shows on page

--- assets/PokeResult.js
+++ assets/PokeResult.js
@@ -1,16 +1,13 @@
-var searchquerry = document.location.search.split("=").pop()
+var searchquerry = document.location.search.split("=").pop();
 searchquerry = searchquerry.toLowerCase();
-var urlhold = 'https://pokeapi.co/api/v2/pokemon/'
-var submiturl = ""
+var urlhold = 'https://pokeapi.co/api/v2/pokemon/';
+var submiturl = "";
+var gifImage = $(".gif-image");
+var TENOR_API_KEY = "AIzaSyAMxg6d2IH5__7rLo8TVcMFPgQKYM7ULPs"
 //console.log(searchquerry)
-
-
-
-
+console.log(gifImage[0].src);
 
 function makeUrl () {submiturl = urlhold + searchquerry, testapi(submiturl)}
-
-
 
 function testapi (submiturl) {
     
@@ -19,31 +16,24 @@ function testapi (submiturl) {
     
     .then(function (response) {
         console.log(response.name), 
-        
-        
-        
-        
-        console.log(response.types[0].type.name); 
+
+        console.log(response.types[0].type.name)
         if (response.types.length > 1) {console.log("its long"), console.log(response.types[1].type.name)}
         else {console.log("Its short")}
         console.log(response)
-    
-    
-    
-    
-    
+
     })
 
 }
 makeUrl()
 
-const TENOR_API_KEY = "AIzaSyAMxg6d2IH5__7rLo8TVcMFPgQKYM7ULPs"
-
 fetch("https://tenor.googleapis.com/v2/search?key="+TENOR_API_KEY+"&q="+searchquerry+"&contentfilter=high&media_filter=gif")
 .then(response => response.json())
 .then (function(data) {
+    var gif = data.results[0].media_formats.gif.url
     console.log(data)
     console.log(data.results[0].url)
     console.log(data.results[0].media_formats.gif.url)
-    // document.getElementById("pokemon-pic").src = data.results[0].media_formats.gif.url
+    gifImage[0].src = gif;
+
 })
