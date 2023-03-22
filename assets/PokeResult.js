9caf282cbdb83089b865e65cb9e3a9be9695d168
GameW1zard <rivenloya@gmail.com>
2023-03-21 20:31:53 -0500

    Update PokeResult.js

--- assets/PokeResult.js
+++ assets/PokeResult.js
@@ -21,23 +21,22 @@ function testapi (submiturl) {
     .then(response => response.json())
     
     .then(function (response) {
-         
-       var spriteurl = response.sprites.front_default    
-
         
+        
+
+        var spriteurl = response.sprites.front_default;    
         console.log(response.name); 
         if (response.types.length > 1) {console.log("the pokemon is ", response.types[0].type.name, " Type and ", response.types[1].type.name, " Type")}
         
-        console.log("the pokemon is ", response.height / 10 .toFixed(1), " meters tall")
-        console.log("the pokemon weighs ", response.weight / 10 .toFixed(1), " Kilograms")
+        console.log("the pokemon is ", response.height / 10 .toFixed(1), " meters tall");
+        console.log("the pokemon weighs ", response.weight / 10 .toFixed(1), " Kilograms");
 
-        console.log(response)
+        console.log(response);
     
-        Sprite.attr("src", spriteurl)
+        Sprite.attr("src", spriteurl);
     
     
     
     })
-
 }
 makeUrl()
